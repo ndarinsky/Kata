93ee589b2431db39288dbf04da90cdebed5324db
nikolai <n.darinsky@gmail.com>
2023-09-05 20:05:35 +0200

    Virtual + infinite scroll works

--- react-coding/virtual-scroll/src/App.js
+++ react-coding/virtual-scroll/src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import ScrolledContainer from './components/ScrolledContainer'
 import React, { useEffect, useState, useRef, useCallback } from "react";
 
 const LIMIT = 20
 const TOTAL_ITEMS_IN_DOM = LIMIT*2
+const ITEM_HEIGHT = 10
 
 const getData = async (offset, count) => {
   await setTimeout(() => {}, 1000)
@@ -12,12 +12,10 @@ const getData = async (offset, count) => {
 
 function App() {
   const [posts, setPosts] = useState([]);
-  const [currentIndex, setCurrentIndex] = useState(LIMIT)
-  const [topPlaceholder, setTopPlaceholder] = useState(0)
+  const [currentIndex, setCurrentIndex] = useState(0)
 
   const topAnchor = useRef()
   const bottomAnchor = useRef()
-  const scrollRef = useRef()
 
   useEffect(() => {
     (async () => {
@@ -30,14 +28,12 @@ function App() {
     const bottomObserver = new IntersectionObserver(async (entries) => {
         if (entries[0].isIntersecting && posts.length){ //don't trigger on 1st render
           const onBottomAnchor = async () => {
-            const chunk = await getData(currentIndex+LIMIT, LIMIT)
+            const chunk = await getData(currentIndex, LIMIT)
             
             const newPosts = [...posts, ...chunk].slice(LIMIT)
 
             setPosts(newPosts)
             setCurrentIndex(prev => prev + LIMIT)
-
-            console.log(newPosts.length)
           }     
 
           await onBottomAnchor()
@@ -45,23 +41,37 @@ function App() {
     })
     bottomObserver.observe(bottomAnchor.current)
 
+    const topObserver = new IntersectionObserver(async (entries) => {
+      if (entries[0].isIntersecting && posts.length && currentIndex>=LIMIT){ //don't trigger on 1st render
+        const onAnchor = async () => {
+          const chunk = await getData(currentIndex+LIMIT, LIMIT)
+          const newPosts = [...chunk, ...posts.slice(LIMIT)]
+
+          setPosts(newPosts)
+          setCurrentIndex(prev => prev - LIMIT)
+
+        }     
+
+        await onAnchor()
+      }
+  })
+    topObserver.observe(topAnchor.current)
+
     return () => {
         bottomObserver.unobserve(bottomAnchor.current)
+        topObserver.unobserve(topAnchor.current)
     }
 }, [posts])
 
   return (
     <div className="App">
         <div className='scrolled-container'>
-            Scrolled Container
-            <div className='anchor' ref={topAnchor}></div>
-            <div className='scrolled-container__list' >
-              <div className='relat' style={{ top: (currentIndex-LIMIT)*10+'px'  }}>
-                { posts.map((post, i) => <div className='item' key={currentIndex-LIMIT+i}> {currentIndex+i}) {post}</div>) }
+            Virtual + Infinite Scrolled List
+              <div className='relative' style={{ top: (currentIndex)*ITEM_HEIGHT+'px'  }}> {/*position:relative + top:dynamic to keep height */}
+                <div className='anchor' ref={topAnchor}></div> {/** top anchor for intersection observer */}
+                { posts.map((post, i) => <div className='item' key={currentIndex+i}> {currentIndex+i}) {post}</div>) }
                 <div className='anchor' ref={bottomAnchor}></div>
               </div>
-            </div>
-            
         </div>
     </div>
   );
